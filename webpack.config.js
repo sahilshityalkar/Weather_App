506f95b5cf6e8b4b31095f7bea297a04eb1af472
sahil <sahilshityalkar05@gmail.com>
2023-07-16 22:27:03 +0530

    add the babel and webpack modules and set the eslint and .prettierrc

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,18 @@
-module: {
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
         test: /\.(?:js|mjs|cjs)$/,
         exclude: /node_modules/,
         use: {
@@ -8,10 +20,20 @@ module: {
           options: {
             presets: [
               ['@babel/preset-env', { targets: "defaults" }]
-            ],
-            plugins: ['@babel/plugin-proposal-class-properties']
+            ]
           }
         }
-      }
-    ]
-  }
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    port: 3000,
+  },
+};
