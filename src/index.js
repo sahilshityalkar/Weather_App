b1a809f52daafa62df510c020e982358341abd3a
sahil <sahilshityalkar05@gmail.com>
2023-07-22 23:48:51 +0530

    remove the js code and add the api key and get the element location and getweatherBtn

--- src/index.js
+++ src/index.js
@@ -2,30 +2,7 @@ const hello = function () {
     return console.log("hello world!");
   };
 
-console.log(hello);
+  const API_KEY = '42f96067a47f4375aed34430231907';
 
-const API_KEY = '42f96067a47f4375aed34430231907';
-
-function getWeaterData(location){
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`;
-
-  fetch(apiUrl)
-  .then(response => {
-      if (!response.ok) {
-          throw new Error('Failed to fetch weather data.');
-      }
-      return response.json();
-  })
-  .then(data => {
-      // Display the weather data in the console
-      console.log(`Weather Data for ${location}:`);
-      console.log(data);
-  })
-  .catch(error => {
-      console.error(error.message);
-  });
-}
-
-// Call the function with a location (e.g., 'New York')
-const location = 'New York';
-getWeatherData(location);
+  const locationInput = document.getElementById('location');
+  const getWeatherBtn = document.getElementById('getWeatherBtn');
