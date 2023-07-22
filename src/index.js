c8bc7e3c741cc238fa804996e58b4b2d261751bb
sahil <sahilshityalkar05@gmail.com>
2023-07-22 23:50:35 +0530

    add the funtion to handel the data from api

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const hello = function () {
     return console.log("hello world!");
-  };
+};
 
-  const API_KEY = '42f96067a47f4375aed34430231907';
+const API_KEY = '42f96067a47f4375aed34430231907';
 
-  const locationInput = document.getElementById('location');
-  const getWeatherBtn = document.getElementById('getWeatherBtn');
+const locationInput = document.getElementById('location');
+const getWeatherBtn = document.getElementById('getWeatherBtn');
 
 
-  function getWeatherData(location) {
+function getWeatherData(location) {
     const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${encodeURIComponent(location)}`;
 
     return fetch(apiUrl)
@@ -19,3 +19,17 @@ const hello = function () {
             return response.json();
         });
 }
+
+async function handleWeatherData(location) {
+    try {
+        const data = await getWeatherData(location);
+        // Display the weather data on the page
+        document.getElementById('locationOutput').textContent = location;
+        document.getElementById('temperatureOutput').textContent = data.current.temp_c + '°C';
+        document.getElementById('timeOutput').textContent = data.location.localtime;
+        document.getElementById('humidityOutput').textContent = data.current.humidity + '%';
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
