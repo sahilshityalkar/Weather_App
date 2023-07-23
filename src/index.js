3c4eef3a1cae78c8bde218ed38bd98a51b279377
sahil <sahilshityalkar05@gmail.com>
2023-07-23 15:05:38 +0530

    get click event to get weather Button

--- src/index.js
+++ src/index.js
@@ -1,50 +1,48 @@
-const hello = function () {
-    return console.log("hello world!");
-};
+        const API_KEY = '42f96067a47f4375aed34430231907';
 
-const API_KEY = '42f96067a47f4375aed34430231907';
+        const locationInput = document.getElementById('location');
+        const getWeatherBtn = document.getElementById('getWeatherBtn');
 
-const locationInput = document.getElementById('location');
-const getWeatherBtn = document.getElementById('getWeatherBtn');
+        function getWeatherData(location) {
+            const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${encodeURIComponent(location)}`;
 
+            return fetch(apiUrl)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Failed to fetch weather data.');
+                    }
+                    return response.json();
+                });
+        }
 
-function getWeatherData(location) {
-    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${encodeURIComponent(location)}`;
-
-    return fetch(apiUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Failed to fetch weather data.');
+        async function handleWeatherData(location) {
+            try {
+                const data = await getWeatherData(location);
+                document.getElementById('locationOutput').textContent = location;
+                document.getElementById('temperatureOutput').textContent = data.current.temp_c + '°C';
+                document.getElementById('timeOutput').textContent = data.location.localtime;
+                document.getElementById('humidityOutput').textContent = data.current.humidity + '%';
+            } catch (error) {
+                console.error(error.message);
             }
-            return response.json();
+        }
+
+        function handleUserInput() {
+            const location = locationInput.value.trim();
+            if (location) {
+                handleWeatherData(location);
+            } else {
+                console.error('Please enter a valid location.');
+            }
+        }
+
+
+        locationInput.addEventListener('keydown', event => {
+            if (event.key === 'Enter') {
+                handleUserInput();
+            }
         });
-}
 
-async function handleWeatherData(location) {
-    try {
-        const data = await getWeatherData(location);
-        // Display the weather data on the page
-        document.getElementById('locationOutput').textContent = location;
-        document.getElementById('temperatureOutput').textContent = data.current.temp_c + '°C';
-        document.getElementById('timeOutput').textContent = data.location.localtime;
-        document.getElementById('humidityOutput').textContent = data.current.humidity + '%';
-    } catch (error) {
-        console.error(error.message);
-    }
-}
+        getWeatherBtn.addEventListener('click', handleUserInput);
 
-function handleUserInput() {
-    const location = locationInput.value.trim();
-    if (location) {
-        handleWeatherData(location);
-    } else {
-        console.error('Please enter a valid location.');
-    }
-}
-
-locationInput.addEventListener('keydown', event => {
-    if (event.key === 'Enter') {
-        handleUserInput();
-    }
-});
-
+        handleWeatherData('Mumbai');
